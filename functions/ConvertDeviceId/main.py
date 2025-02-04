620017481307dd278b695278a93a19abd5a9c8e8
Gax <gax.theodorio@crowdstrike.com>
2025-02-04 11:46:03 -0500

    Worklow changes

--- functions/ConvertDeviceId/main.py
+++ functions/ConvertDeviceId/main.py
@@ -8,6 +8,7 @@ func = Function.instance()
 @func.handler(method="POST", path="/convert_host_id")
 def convert_host_id(request: Request, config: [Dict[str, any], None]) -> Response:
     device_id = request.body.get("host_id")
+
     body = {"device_id": str(uuid.UUID(hex=device_id))}
     return Response(
         body=body,
